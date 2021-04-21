7a6961f08665902069da474eac44772d3bc1c684
Rafael <rafaelgoncalvesbarreira@gmail.com>
2021-04-20 22:54:44 -0300

    pasta de testes

---
+++ tests/TechChallenge.Application.Test/Services/CardServicesTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TechChallenge.Application.Test.Services
+{
+    public class CardServicesTest
+    {
+        [Fact]
+        public void ValidateTokenTrue()
+        {
+
+        }
+
+        [Fact]
+        public void ValidateTokenFalse()
+        {
+
+        }
+
+
+
+        public void GenerateTokens()
+        {
+
+        }
+    }
+}
